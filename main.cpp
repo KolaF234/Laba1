b9c5c6df1d8c78d2c755b5590242a94cd036c663
KolaF234 <Nikolaidalinger@gmail.com>
2024-12-05 17:09:21 +0500

    second commit

--- main.cpp
+++ main.cpp
@@ -11,24 +11,42 @@ vector<string> fileRead(){
     ifstream file("KolaF.txt");
     if(file.is_open()){
         while(getline(file, s)){
-            array.push_back(s) ;
+        array.push_back(s) ;
         }
     }
     file.close();
     cout << array.size();
     return array;
 }
-void fileOutput(){
-
+void fileOutput(vector<string> &array){
+    ofstream out;
+    out.open("hello.txt");
+  if (out.is_open())
+  {
+      for(size_t i = 0; i < array.size(); i++){
+                out << array[i] << endl;
+      }
+  }
+  out.close();
 }
 
-void print(){
-
+void print(vector<string> array){
+    for(size_t i = 0; i < array.size(); i++){
+        cout << array[i] << endl;
+    }
+}
+void sign (vector<string> array){
 }
 
 int main()
 {
 
     vector<string> array = fileRead();
-    return 0;
+        //cout << array.size();
+    fileOutput(array);
+   // cout << array.size();
+    print(array);
+
+
+return 0;
 }
